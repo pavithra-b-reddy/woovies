c9b2f9bf8fe1dc284e8f6120394c7349b58124e6
Pavithra Reddy <pavithrabrv@gmail.com>
2020-10-31 23:32:39 -0400

    Create json objects

---
+++ resources/movies.js
@@ -0,0 +1,14 @@
+var inception = {'name':'Inception', 'year':2010, 'director': 'Christopher Nolan',
+ 'desc':'Inception Description', 'on_watchlist':0, 'genre':'Sci-Fi'}
+
+var endgame = {'name':"Avengers: Endgame", 'year':2019, 'director':'Anthony Russo and Joe Russ',
+'desc':'Endgame description', 'on_watchlist':0, 'genre':'Action'}
+
+var joker = {'name':'Joker', 'year':2019, 'director':'Todd Phillips',
+'desc':'Joker description', 'on_watchlist':0, 'genre':'Thriller'}
+
+var split = {'name':'Split', 'year':2016, 'director':'M. Night Shyamalan',
+'desc':'Split description', 'on_watchlist':0, 'genre':'Thriller'}
+
+var jurassic = {'name':'Jurassic Park', 'year':1993, 'director':'Steven Spielberg',
+'desc':'Jurassic Park description', 'on_watchlist':0, 'genre':'Sci-Fi'}
