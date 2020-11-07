218a903dafaa6e618d7abe5d47fd7d863bb15e96
Pavithra Reddy <pavithrabrv@gmail.com>
2020-11-07 13:22:24 -0500

    Updates

--- Woo-vies/resources/movies.js
+++ Woo-vies/resources/movies.js
@@ -1,14 +1,19 @@
 var inception = {'name':'Inception', 'year':2010, 'director': 'Christopher Nolan',
- 'desc':'Inception Description', 'on_watchlist':0, 'genre':'Sci-Fi'}
+ 'desc':'Inception Description', 'on_watchlist':0, 'genre':'Sci-Fi',
+ 'poster':'./assets/posters/inception.jpg'}
 
 var endgame = {'name':"Avengers: Endgame", 'year':2019, 'director':'Anthony Russo and Joe Russo',
-'desc':'Endgame description', 'on_watchlist':0, 'genre':'Action'}
+'desc':'Endgame description', 'on_watchlist':0, 'genre':'Action',
+'poster':'./assets/posters/endgame.jpg'}
 
 var joker = {'name':'Joker', 'year':2019, 'director':'Todd Phillips',
-'desc':'Joker description', 'on_watchlist':0, 'genre':'Thriller'}
+'desc':'Joker description', 'on_watchlist':0, 'genre':'Thriller',
+'poster':'./assets/posters/joker.jpg'}
 
 var split = {'name':'Split', 'year':2016, 'director':'M. Night Shyamalan',
-'desc':'Split description', 'on_watchlist':0, 'genre':'Thriller'}
+'desc':'Split description', 'on_watchlist':0, 'genre':'Thriller',
+'poster':'./assets/posters/split.jpg'}
 
 var jurassic = {'name':'Jurassic Park', 'year':1993, 'director':'Steven Spielberg',
-'desc':'Jurassic Park description', 'on_watchlist':0, 'genre':'Sci-Fi'}
+'desc':'Jurassic Park description', 'on_watchlist':0, 'genre':'Sci-Fi',
+'poster':'./assets/posters/jurassic.jpg'}
